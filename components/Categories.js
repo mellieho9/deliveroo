cde5a038e4e02be4577c3bcddef3e47e3ea1edce
Meliora Ho <85920674+mellieho9@users.noreply.github.com>
2022-12-22 00:02:50 -0500

    uploaded folder

---
+++ components/Categories.js
@@ -0,0 +1,32 @@
+import { View, Text, ScrollView } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import CategoryCard from './CategoryCard'
+import sanityClient from "../sanity"
+import { urlFor } from '../sanity'
+
+const Categories = () => {
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    sanityClient.fetch(`*[_type == "category"]`)
+    .then((data) => {
+    setCategories(data);
+  });
+}, []);
+  return (
+    <ScrollView contentContainerStyle={{
+        paddingHorizontal: 15,
+        paddingTop: 10,
+    }} horizontal showsHorizontalScrollIndicator={false}>
+      {categories.map((category) => (
+        <CategoryCard
+        key={category._id}
+        imgUrl={urlFor(category.image).url()}
+        title={category.name}
+        />
+      ))}
+        
+    </ScrollView>
+  )
+}
+
+export default Categories
